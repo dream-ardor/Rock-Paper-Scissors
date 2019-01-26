6801072c1146a2397f51aac519cac3daa2ae806d
Dan Barrick <barrick.dan80@gmail.com>
2019-01-25 17:04:06 -0800

    create outline in html and js files

---
+++ script.js
@@ -0,0 +1,11 @@
+function computerPlay(){
+    return computerSelection(Math.random)
+}
+
+function playRound(playerSelection, computerSelection) {
+    return " "
+}
+
+function game(playRound){
+
+}
